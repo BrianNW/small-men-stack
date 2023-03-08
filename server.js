af7eb4dd7986aeea2d2b200452b1ed71ea9a3e0e
Brian <briantechdom@gmail.com>
2023-03-08 13:22:49 -0800

    Add Mongoose db connection

--- server.js
+++ server.js
@@ -4,5 +4,13 @@
 const express = require('express')
 // Create an app variable that will allow us  to run express function which we can use to configure our server
 const app = express()
+// Require in Mongoose for our db
+const mongoose = require('mongoose')
+
+// Connect to our db
+mongoose.connect('mongodb://localhost/subscribers', {useNewUrlParser: true})
+const db = mongoose.connection
+
 // Create app.listen to run a server on port 3000 and console log it
 app.listen(3000, ()=> console.log('Server running'))
+//run > npm run devStart to run this server

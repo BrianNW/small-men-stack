70d63291bf2be879647d59ca920dd65e48d1c2fd
Brian <briantechdom@gmail.com>
2023-03-08 13:07:41 -0800

    Add notes

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-const express = require('express')
-const app = express()
+//This small CRUD app serves as a template for future Node, Express, MongoDB based projects.
 
+// Create a variable that will require in the Express library
+const express = require('express')
+// Create an app variable that will allow us  to run express function which we can use to configure our server
+const app = express()
+// Create app.listen to run a server on port 3000 and console log it
 app.listen(3000, ()=> console.log('Server running'))

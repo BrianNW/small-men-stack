cc7d9e3468ec4b2cfe2092c5fce5f5945681d0bd
Brian <briantechdom@gmail.com>
2023-03-10 13:13:23 -0800

    Add get all, get one, create one, update(patch) one, delete one routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,34 @@
 const express = require('express')
 const router = express.Router()
 
+// Getting all
+router.get('/', (req, res) => {
+
+})
+
+// Getting one
+
+router.get('/:id', (req, res) => {
+    req.params.id
+})
 module.exports = router
+
+
+// Creating one
+
+router.post('/', (req, res) => {
+    
+})
+
+// Updating one
+// Use .patch rather than put to update only ONE entry of this subscriber that gets passed, not all the information at once
+router.patch('/:id', (req, res) => {
+    req.params.id
+})
+
+
+// Delete One
+
+router.delete('/:id', (req, res) => {
+    
+})

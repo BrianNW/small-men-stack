d48b3e546f0dc47da590beff8b910fedd0dd3aa2
Brian <briantechdom@gmail.com>
2023-04-24 11:10:36 -0700

    Update update and delete post with middleware

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -37,13 +37,13 @@ router.post('/', async (req, res) => {
 
 // UPDATE ONE
 // Use .patch rather than put to update only ONE entry of this subscriber that gets passed, not all the information at once
-router.patch('/:id', (req, res) => {
+router.patch('/:id', getSubscriber, (req, res) => {
     res.subscriber
 })
 
 
 // DELETE ONE
-router.delete('/:id', (req, res) => {
+router.delete('/:id', getSubscriber, (req, res) => {
     res.subscriber
 })
 
